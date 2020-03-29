837c659f1cf5b96830db82ba4956f004e786b3c7
Emin <israfilzadehemin@gmail.com>
2020-03-29 23:13:52 +0400

    Homework 13 reader issue solved

--- src/main/java/hw13/dao/CollectionFamilyDao.java
+++ src/main/java/hw13/dao/CollectionFamilyDao.java
@@ -64,7 +64,6 @@ public class CollectionFamilyDao implements DAO<Family> {
     File file = new File("src/main/java/hw13/database/db.txt");
 
     try {
-      new BufferedReader(new FileReader(file)).lines().collect(Collectors.toList());
       BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 
       for (Family family : familyList) {
